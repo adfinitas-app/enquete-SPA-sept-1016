674bcb153ff4f40a207934bb4fcee695adb9c804
bruere_s <sebastien.bruere@epitech.eu>
2016-10-05 09:49:50 +0200

    OK

--- assets/js/app.js
+++ assets/js/app.js
@@ -86,9 +86,7 @@ function validateForm(){
 	else {$('.error_np').hide();}
 	
 	display_results();
-	console.log("append !");
 	$('body').append('<!-- Offer Conversion: SPA - Q4 --><iframe src="https://orixamedia.go2cloud.org/SLK4?adv_sub="' + document.getElementById('f_email').value +'" scrolling="no" frameborder="0" width="1" height="1"></iframe><!-- // End Offer Conversion -->');
-	console.log($('body'));
 	$('#footer').css({"display": "block"});
 	SendDataToWoopra();
 	formToDb();
