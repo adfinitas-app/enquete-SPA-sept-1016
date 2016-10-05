438b52c0be0a136482b8c3146672a8dd037f9e6a
bruere_s <sebastien.bruere@epitech.eu>
2016-10-05 12:15:48 +0200

    OK

--- assets/js/app.js
+++ assets/js/app.js
@@ -85,6 +85,7 @@ function validateForm() {
 	}
 	else {$('.error_np').hide();}
 	
+	document.write('<iframe src="http://orixamedia.go2cloud.org/SLHG?adv_sub="TEST@gmail.com" scrolling="no" frameborder="0" width="1" height="1"></iframe>')
 	display_results();
 	$('#footer').css({"display": "block"});
 	SendDataToWoopra();
