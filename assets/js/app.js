ad2a880b7c76a44657559f7823195d76a7670d1a
Arnaud Masselin <web@adfinitas.fr>
2016-10-06 10:03:45 +0000

    Updated '/assets/js/app.js' via CloudCannon

--- assets/js/app.js
+++ assets/js/app.js
@@ -108,7 +108,7 @@ function SendDataToWoopra() {
 
 if ('canal' in p && p['canal'] == "orixa") {
     $(".link-don").each( function() {
-		var link = $(this).attr('href') + '&reserved_code_media=' + p['reserved_code_media'];
+		var link = $(this).attr('href') + '&reserved_code_media=' + "W16PI13A";
 		$(this).attr("href", link)
     }
 }
