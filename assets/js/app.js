3a060a9b236670955af1cd6d9a045af063306033
Arnaud Masselin <web@adfinitas.fr>
2016-10-19 10:25:23 +0000

    Updated 'assets/js/app.js' via CloudCannon

--- assets/js/app.js
+++ assets/js/app.js
@@ -69,7 +69,14 @@ function remontéOrixaBis {
 }
 
 function remontéFb() {
-    
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1413104348730186');
+fbq('track', 'Confirmation');
+$("body").append('<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1413104348730186&ev=PageView&noscript=1"/>');
 }
 
 $('.stop-sticky').click(function () {
